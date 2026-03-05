d4dc7433dd3e707bef2972c94e0598b35366d109
usm3361 <usm3361@gmail.com>
2026-03-05 17:14:35 +0200

    The End

--- Frontend/src/components/MovieCard.jsx
+++ Frontend/src/components/MovieCard.jsx
@@ -5,9 +5,10 @@ const MovieCard = ({movie}) => {
   return (
     <div className='movie-card'>
       <img src={movie.Images[0]} alt="" />
-      <h3>{movie.title}</h3>
+      <h2>ID: {movie.imdbID}</h2>
+      <h3>Title: {movie.Title}</h3>
       <p>Year: {movie.Year}</p>
-      <p>Rating:{movie.imdbRating}</p>
+      <p>Rating: {movie.imdbRating}</p>
       <Link to={`/movie/${movie.imdbID}`}>View Details</Link>
     </div>
   )
